3ce0f895ccc975edfd2129995fd65f3f1235b3d2
Develing1991 <completed0728@gmail.com>
2023-04-09 22:44:54 +0900

    we need many refactoring and condition, but just simple doing now

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+const { makes } = useCars();
 const route = useRoute();
+const router = useRouter();
 const modal = ref<Record<string, boolean>>({
   make: false,
   location: false,
   price: false,
 });
 const city = ref("");
+const priceRange = ref({
+  min: "",
+  max: "",
+});
+const priceRangeText = computed(() => {
+  const { minPrice, maxPrice } = route.query;
+  if (!minPrice && !maxPrice) return "Any";
+  else if (!minPrice && maxPrice) {
+    return `< $${maxPrice}`;
+  } else if (minPrice && !maxPrice) {
+    return `> $${minPrice}`;
+  } else {
+    return `$${minPrice}-$${maxPrice}`;
+  }
+});
 const updateModal = (key: string) => {
   // const updateModal = (key: string, off?: boolean) => {
   // modal.value[key] = off ? false: !modal.value[key];
@@ -25,10 +42,27 @@ const onChangeLocation = () => {
   navigateTo(`/city/${city.value}/car/${route.params.make}`);
   city.value = "";
 };
+const onChangeMake = (make: string) => {
+  updateModal("make");
+  navigateTo(`/city/${route.params.city}/car/${make}`);
+};
+const onChangePrice = () => {
+  updateModal("price");
+  if (priceRange.value.max && priceRange.value.min) {
+    if (priceRange.value.min > priceRange.value.max) return;
+  }
+  router.push({
+    query: {
+      minPrice: priceRange.value.min,
+      maxPrice: priceRange.value.max,
+    },
+  });
+};
 </script>
 <template>
   <!-- CAR SIDE BAR -->
   <div class="shadow border w-96 mr-10 z-30 h-[190px]">
+    <!-- LOCATION -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Location</h3>
       <h3 class="text-blue-400 capitalize" @click="updateModal('location')">
@@ -47,14 +81,59 @@ const onChangeLocation = () => {
         </button>
       </div>
     </div>
+    <!-- LOCATION -->
+    <!-- MAKE -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Make</h3>
-      <h3 class="text-blue-400 capitalize">toyota</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('make')">
+        {{ route.params.make || "Any" }}
+      </h3>
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+        v-if="modal.make"
+      >
+        <h4
+          v-for="make in makes"
+          :key="make"
+          class="w-1/3"
+          @click="onChangeMake(make)"
+        >
+          {{ make }}
+        </h4>
+      </div>
     </div>
+    <!-- MAKE -->
+    <!-- PRICE -->
     <div class="p-5 flex justify-between relative cursor-pointer border-b">
       <h3>Price</h3>
-      <h3 class="text-blue-400 capitalize">any</h3>
+      <h3 class="text-blue-400 capitalize" @click="updateModal('price')">
+        {{ priceRangeText }}
+      </h3>
+      <div
+        class="absolute border shadow left-56 p-5 top-1 -m-1 bg-white"
+        v-if="modal.price"
+      >
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Min"
+          v-model="priceRange.min"
+        />
+        <input
+          class="border p-1 rounded"
+          type="number"
+          placeholder="Max"
+          v-model="priceRange.max"
+        />
+        <button
+          class="bg-blue-400 w-full mt-2 rounded text-white p-1"
+          @click="onChangePrice"
+        >
+          Apply
+        </button>
+      </div>
     </div>
+    <!-- PRICE -->
   </div>
   <!-- CAR SIDE BAR -->
 </template>
--- composables/useCars.ts
+++ composables/useCars.ts
@@ -1,9 +1,12 @@
 import cars from "@/data/cars.json";
+import makes from "@/data/makes.json";
 import { Cars } from "@/types/Car";
 
 export const useCars = () => {
   cars as Cars;
+  makes as string[];
   return {
     cars,
+    makes,
   };
 };

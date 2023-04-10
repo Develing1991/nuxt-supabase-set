67f7ed50aa396c21e07e64ecaa700127234c2b77
Develing1991 <completed0728@gmail.com>
2023-04-11 03:23:13 +0900

    refactor messageCard

--- components/Car/MessageCard.vue
+++ components/Car/MessageCard.vue
@@ -1,13 +1,27 @@
+<script setup lang="ts">
+const { message } = defineProps<Props>();
+</script>
 <template>
   <div class="even:bg-gray-100 p-4">
-    <h1 class="font-bold text-2xl">Suhan lee</h1>
+    <h1 class="font-bold text-2xl">{{ message.name }}</h1>
     <p class="text-gray-700">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde corporis,
-      eligendi deleniti maxime officia beatae earum nulla? Porro, iure animi?
+      {{ message.message }}
     </p>
     <div class="flex items-center mt-4">
-      <p class="text-green-700 mr-10">lee@naver.com</p>
-      <div class="text-blue-500">555-555-5555</div>
+      <p class="text-green-700 mr-10">{{ message.email }}</p>
+      <div class="text-blue-500">{{ message.phone }}</div>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+interface Props {
+  message: Message;
+}
+type Message = {
+  message: string;
+  name: string;
+  email: string;
+  phone: string;
+};
+</script>
--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -3,12 +3,20 @@ definePageMeta({
   layout: "custom",
   middleware: ["auth"],
 });
+const route = useRoute();
+const { data: messages } = useFetch(
+  `/api/car/listings/${route.params.id}/message`,
+  {
+    method: "GET",
+  }
+);
 </script>
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    <CarMessageCard
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    />
   </div>
 </template>
